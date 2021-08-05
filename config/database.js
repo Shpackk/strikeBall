4995398fbaf0f7aa3056fbcdf8566722312ac0da
Shpackk <victorija45@gmail.com>
2021-08-05 23:36:56 +0300

    new db config to support bounds

--- config/database.js
+++ config/database.js
@@ -1,15 +1,12 @@
-require('dotenv').config()
-const Sequelize = require('sequelize')
+// require('dotenv').config()
+// const Sequelize = require('sequelize')
 
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        dialect: process.env.DB_DIALECT,
-        host: process.env.HOST
-    }
-)
-
-// dotenv (app.js) check avail.
-// 
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASS,
+//     {
+//         dialect: process.env.DB_DIALECT,
+//         host: process.env.HOST
+//     }
+// )
--- config/passportJWT.js
+++ config/passportJWT.js
@@ -2,6 +2,7 @@ const passport = require('passport')
 const passportJWT = require("passport-jwt");
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = require('passport-jwt').ExtractJwt
+require('dotenv').config()
 
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromHeader('token'),
