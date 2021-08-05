d050ddac182c57d79f8c9e91bf945b793e345682
Shpackk <victorija45@gmail.com>
2021-08-05 23:35:43 +0300

    manager registration request added

--- userDTO/userDBrequests.js
+++ userDTO/userDBrequests.js
@@ -1,6 +1,7 @@
-const User = require('../models/User')
+const { request } = require('express')
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
+const { User, Request } = require('../models/index')
 
 // for users JWT signup
 async function createUser(name, role, password, email) {
@@ -10,7 +11,6 @@ async function createUser(name, role, password, email) {
         role,
         password,
     })
-
 }
 
 //for all users view
@@ -20,12 +20,16 @@ async function findAllUsers() {
 
 //find all managers
 async function findAllManagers() {
-    return await User.findAll({
-        where: {
-            role: 'manager'
-        },
-        attributes: ['id', 'email', 'name', 'role']
-    })
+    try {
+        return await User.findAll({
+            where: {
+                role: 'manager'
+            },
+            attributes: ['id', 'email', 'name', 'role']
+        })
+    } catch (error) {
+        return error
+    }
 }
 
 
@@ -65,12 +69,17 @@ async function updateUser(name, id) {
 }
 
 // check if user is already registered 
-async function findOneUser(user) {
-    return await User.findOne({
-        where: {
-            [Op.or]: [{ name: user.name }, { email: user.email }]
-        }
-    })
+async function findOneUser(name, email) {
+
+    try {
+        return await User.findOne({
+            where: {
+                [Op.or]: [{ name }, { email }]
+            }
+        })
+    } catch (error) {
+        return error
+    }
 }
 
 //for loging in 
@@ -112,9 +121,83 @@ async function updatePassword(id, password) {
     })
 }
 
+// create request for manager's registration 
+async function newRequest(user, type) {
+    try {
+        const found = await Request.findOne({
+            where: {
+                userEmail: user.email
+            }
+        })
+        if (found) {
+            const error = {
+                "msg": "applied"
+            }
+            throw error
+        }
+        const createdRequest = await Request.create({
+            status: 'active',
+            userEmail: user.email,
+            userName: user.name,
+            userPass: user.password,
+            requestType: type
+        })
+        if (createdRequest) {
+            return "You succesfully applied! We will approve and send you an email"
+        }
+    } catch (error) {
+        throw error
+    }
+}
+
+async function extractRequests() {
+    try {
+        const requests = await Request.findAll({
+            //if we delete request after decission, no sense to pass 'approved' column
+            attributes: ['id', 'status', 'userEmail', 'userName', 'requestType']
+        })
+        return requests
+    } catch (error) {
+        throw error
+    }
+}
+
+async function findRequest(reqId) {
+    try {
+        return await Request.findOne({
+            where: {
+                id: reqId
+            },
+            attributes: ['userEmail', 'userName', 'userPass']
+        })
+    } catch (error) {
+        throw error
+    }
+}
+
+async function acceptRequest(reqId, userName, password, email) {
+    try {
+        await deleteRequest(reqId)
+        return await createUser(userName, 'manager', password, email)
+    } catch (error) {
+        throw error
+    }
+}
+
+async function deleteRequest(reqId) {
+    return await Request.destroy({
+        where: {
+            id: reqId
+        }
+    })
+}
+
+
+
 
 module.exports = {
     createUser, deleteUser, updateUser, findOneById, findOneUser,
     createUserGoogle, findAllUsers, findOneByName, findAllManagers,
-    findOneByEmail, updatePassword
+    findOneByEmail, updatePassword, newRequest, extractRequests,
+    acceptRequest, deleteRequest, findRequest
 }
