d010a5c5d14a71b8fda15342308b50a0eaa7e83c
Shpackk <victorija45@gmail.com>
2021-08-09 20:21:22 +0300

    ban requests to DB

---
+++ banDTO/banRequests.js
@@ -0,0 +1,41 @@
+const { Banlist, User } = require('../models/index')
+
+async function isBanned(userEmail) {
+    try {
+        return await Banlist.findOne({
+            where: {
+                userEmail: userEmail
+            }
+        })
+    } catch (error) {
+        throw error
+    }
+}
+
+
+
+async function banUser(userId, description, userEmail) {
+    try {
+        return await Banlist.create({
+            userId,
+            description,
+            userEmail
+        })
+    } catch (error) {
+        throw error
+    }
+}
+
+async function unbanUser(userId) {
+    try {
+        return await Banlist.destroy({
+            where: {
+                userId
+            }
+        })
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = { banUser, isBanned, unbanUser }
