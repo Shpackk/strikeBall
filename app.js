fb2e3538fcdcda64d44ed3029a5ce632c9bf772d
Shpackk <victorija45@gmail.com>
2021-08-11 19:05:58 +0300

    static file path added

--- app.js
+++ app.js
@@ -7,18 +7,23 @@ const userRoutes = require('./routes/UserRoutes')
 const teamRoutes = require('./routes/TeamRoutes')
 const authRoutes = require('./routes/authRoutes')
 const errorHandler = require('./middleware/errorHandler')
+const path = require('path')
+// const { storageConfig, fileFilter } = require('./service/multer')
 require('dotenv').config()
 
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(passport.initialize())
-app.use(express.static('static'))
+// app.use(express.static('static'))
+app.use('uploads/', express.static(path.join(__dirname, 'uploads')))
 app.use(express.json())
 app.use(userRoutes)
 app.use(teamRoutes)
 app.use(authRoutes)
 app.use(errorHandler)
+// app.use(multer({ storage: storageConfig, fileFilter }).single("filedata"));
+
 
 db.sequelize.sync()
     .then(() => console.log('DB is synced!'))
