13a3394cd2db8fb93ab6b2a800a56b88e107b273
Shpackk <victorija45@gmail.com>
2021-08-18 19:35:38 +0300

    check for test env

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 const express = require('express')
 const app = express()
 const passport = require('passport')
-const PORT = process.env.PORT || 3000
 const db = require('./models/index')
 const userRoutes = require('./routes/UserRoutes')
 const teamRoutes = require('./routes/TeamRoutes')
 const authRoutes = require('./routes/authRoutes')
 const errorHandler = require('./middleware/errorHandler')
 const path = require('path')
-// const { storageConfig, fileFilter } = require('./service/multer')
 require('dotenv').config()
+const PORT = process.env.PORT || 3000
 
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(passport.initialize())
-// app.use(express.static('static'))
 app.use('uploads/', express.static(path.join(__dirname, 'uploads')))
 app.use(express.json())
 app.use(userRoutes)
 app.use(teamRoutes)
 app.use(authRoutes)
 app.use(errorHandler)
-// app.use(multer({ storage: storageConfig, fileFilter }).single("filedata"));
-
+app.get('*', function (req, res) {
+    res.status(404).json({ "message": "Page Not Found" })
+});
 
 db.sequelize.sync()
     .then(() => console.log('DB is synced!'))
     .catch(err => console.log(err))
 
 // app listening 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
-
+// app.listen(PORT)
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT);
+}
+module.exports = app
