f6fa67ea954bfdb6828f865b257b1a88c106f0d9
Shpackk <victorija45@gmail.com>
2021-08-27 19:35:57 +0300

    bug fixes, password controller functionality added

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt')
+const passControl = require('../DTO/userDTO/passwordControll')
 const dbRequest = require('../DTO/userDTO/userDBrequests')
 const banDbRequest = require('../DTO/banDTO/banRequests')
 const token = require('../DTO/userDTO/userTokenControll')
@@ -8,20 +8,20 @@ const rolesDbRequest = require('../DTO/rolesDTO/rolesDBrequests')
 
 
 module.exports.createUser = async (req, res, next) => {
+    const user = {
+        name: req.body.name.toLowerCase(),
+        email: req.body.email.toLowerCase(),
+        role: req.body.role,
+        password: req.body.password
+    }
     try {
-        const user = {
-            name: req.body.name.toLowerCase(),
-            email: req.body.email.toLowerCase(),
-            role: req.body.role,
-            password: req.body.password
-        }
         const picturePath = req.file ? req.file.path : ''
         check.inputValidation(user)
         const checkUser = await dbRequest.findOneUser(user.name, user.email)
         if (!checkUser) {
             const roleId = await rolesDbRequest.findRole(user.role)
             if (user.role == 'user') {
-                const hashedPassword = await bcrypt.hash(user.password, 10)
+                const hashedPassword = await passControl.hash(user.password, 10)
                 const userDB = await dbRequest.createUser(user.name, roleId, hashedPassword, user.email, picturePath)
                 const accessToken = token.sign(userDB.dataValues)
                 res.status(200).json({
@@ -33,17 +33,14 @@ module.exports.createUser = async (req, res, next) => {
                 })
             }
             if (user.role == 'manager') {
-                user.password = await bcrypt.hash(user.password, 10)
+                user.password = await passControl.hash(user.password, 10)
                 await dbRequest.newRequest(user, 'manager registration')
-                socket.notificationForAdmin('New Manager Registration')
+                await socket.notificationForAdmin('New Manager Registration')
                 res.status(201).json({
                     "message": 'You sucessfully applied!'
                 })
             }
-        } else {
-            error = { "status": 409 }
-            throw error
-        }
+        } else next({ "status": 409 })
     } catch (error) {
         next(error)
     }
@@ -54,28 +51,23 @@ module.exports.loginUser = async (req, res, next) => {
     try {
         check.inputValidation(user)
         const userDB = await dbRequest.findOneByName(user.name)
-        if (userDB == null) {
-            const error = { status: 404 }
-            throw error
-        }
+        if (userDB == null)
+            throw { status: 404 }
+
         const isBanned = await banDbRequest.isBanned(userDB.email)
         if (isBanned != null) {
             res.status(403).json({ "message": `${userDB.email}, we're sorry, but you are banned from our service` })
-        }
-        const auth = await bcrypt.compare(user.password, userDB.password)
-        if (auth) {
-            const accessToken = token.sign(userDB.dataValues)
-            res.json({
-                id: userDB.id,
-                name: userDB.name,
-                roleId: userDB.RoleId,
-                token: accessToken
-            }).status(201)
         } else {
-            const error = {
-                "msg": "wrong password"
-            }
-            throw error
+            const auth = await passControl.compare(user.password, userDB.password)
+            if (auth) {
+                const accessToken = token.sign(userDB.dataValues)
+                res.status(201).json({
+                    id: userDB.id,
+                    name: userDB.name,
+                    roleId: userDB.RoleId,
+                    token: accessToken
+                })
+            } else next({ "msg": "wrong password" })
         }
     } catch (error) {
         next(error)
