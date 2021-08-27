31b987ca6068837d92a599133f5a285693bb27d8
Shpackk <victorija45@gmail.com>
2021-08-27 19:32:27 +0300

    adding return to all responses to avoid further code running

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,62 +1,65 @@
 function errorHandler(err, req, res, next) {
     // status codes err
     if (err.status === 409) {
-        res.status(409).json({ "message": "user is already registered" })
+        return res.status(409).json({ "message": "user is already registered" })
     }
     if (err.status === 404) {
-        res.status(404).json({ "message": "Not Found" })
+        return res.status(404).json({ "message": "Not Found" })
     }
     if (err.status === 500) {
-        res.status(500).json({ "message": "Server error, please try again" })
+        return res.status(500).json({ "message": "Server error, please try again" })
     }
     // msg errors ( fields verify)
     if (err.msg == "Bad token") {
-        res.status(409).json({ "message": "Invalid token or token is expired. Please re-login" })
+        return res.status(409).json({ "message": "Invalid token or token is expired. Please re-login" })
     }
     if (err.msg == "role") {
-        res.status(400).json({ "message": "Enter correct role" })
+        return res.status(400).json({ "message": "Enter correct role" })
     }
     if (err.msg == "password") {
-        res.status(400).json({ "message": "Password should be greater than 5" })
+        return res.status(400).json({ "message": "Password should be greater than 5" })
     }
     if (err.msg == "name") {
-        res.status(400).json({ "message": "Name should be in lowerCase and greater than 4" })
+        return res.status(400).json({ "message": "Name should be in lowerCase and greater than 4" })
     }
     if (err.msg == "email") {
-        res.status(400).json({ "message": "Bad Email" })
+        return res.status(400).json({ "message": "Bad Email" })
     }
     if (err.msg == "wrong password") {
-        res.status(400).json({
+        return res.status(400).json({
             "message": "You passed wrong password",
             "restoreLink": "localhost:3000/user/forgot-password"
         })
     }
     if (err.msg == "missmatch") {
-        res.status(400).json({ "message": "Passwords did not match" })
+        return res.status(400).json({ "message": "Passwords did not match" })
     }
     if (err.msg == "managers") {
-        res.status(404).json({ "message": "No managers yet" })
+        return res.status(404).json({ "message": "No managers yet" })
     }
     if (err.msg == "applied") {
-        res.status(409).json({ "message": "You already applied. Please wait until we approve." })
+        return res.status(409).json({ "message": "You already applied. Please wait until we approve." })
     }
     if (err.msg == 'already in team') {
-        res.status(409).json({ "message": "You already in this team. Switch team or request for leaving" })
+        return res.status(409).json({ "message": "You already in this team. Switch team or request for leaving" })
     }
     if (err.msg == "not in team") {
-        res.status(409).json({ "message": "You can't leave team you are not in" })
+        return res.status(409).json({ "message": "You can't leave team you are not in" })
     }
     if (err.msg == 'teamkick') {
-        res.status(404).json({ "message": "User not found or he is not in this team" })
+        return res.status(404).json({ "message": "User not found or he is not in this team" })
     }
     if (err.msg == 'taken name') {
-        res.status(409).json({ "message": "This name is already taken" })
+        return res.status(409).json({ "message": "This name is already taken" })
     }
     if (err.msg == 'No such team') {
-        res.status(404).json({ "message": "This team doesnt exist" })
+        return res.status(404).json({ "message": "This team doesnt exist" })
     }
     if (err.msg == "Filetype") {
-        res.status(409).json({ "message": "Non-supported filetype. Pick PNG,JPG,JPEG" })
+        return res.status(409).json({ "message": "Non-supported filetype. Pick PNG,JPG,JPEG" })
+    }
+    else {
+        console.log(err)
     }
 }
 
