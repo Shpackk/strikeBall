c24cc9744379133969dc7a4bd317cf23786338ad
Shpackk <victorija45@gmail.com>
2021-08-30 19:53:09 +0300

    data for creating test users

---
+++ tests/testUsersData.js
@@ -0,0 +1,37 @@
+
+const testManager = {
+    email: 'onemore@gmail.com',
+    name: 'onemore',
+    RoleId: 2,
+    password: '123456',
+    picture: null
+}
+
+const testUser = {
+    email: 'bannedusertest@gmail.com',
+    name: 'bannedusertest',
+    RoleId: 1,
+    password: '123456',
+    picture: null
+}
+
+const testUserTwo = {
+    name: "testfromjest",
+    email: "testfromjest@google.com",
+    role: "user",
+    password: '123456',
+    picture: null
+}
+
+const AnotherUser = {
+    name: 'fortestpurpose',
+    email: 'fortestpurpose@gmail.com',
+    role: 'user',
+    password: 'admin'
+}
+const adminCreds = {
+    name: 'admin',
+    password: 'admin'
+}
+
+module.exports = { testManager, testUser, testUserTwo, AnotherUser, adminCreds }
