7b748557a860a01ecc32e7a4c5c0b39305d3b613
Shpackk <victorija45@gmail.com>
2021-08-30 19:52:38 +0300

    service for saving logs

---
+++ service/mongoLogsSaver.js
@@ -0,0 +1,17 @@
+const Logs = require('../models/logs')
+
+async function save(name, method, url, body) {
+    try {
+        const log = {
+            user: name,
+            method,
+            url,
+            requestBody: JSON.stringify(body)
+        }
+        await Logs.create(log)
+    } catch (error) {
+        console.log('log save err', error)
+    }
+}
+
+module.exports = { save }
