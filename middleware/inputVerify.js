82a59c6dd90b19458827ec64f9ee174ac8a3b37a
Shpackk <victorija45@gmail.com>
2021-09-07 18:37:44 +0300

    if statement merged

--- middleware/inputVerify.js
+++ middleware/inputVerify.js
@@ -9,20 +9,14 @@ function inputValidation(user) {
 
 function badInput(user) {
     const namePattern = /[a-z0-9]{2,}/
-    if (user.hasOwnProperty('name')) {
-        if (namePattern.test(user.name) == false) {
-            return "name"
-        }
+    if ((user.hasOwnProperty('name')) && (namePattern.test(user.name) == false)) {
+        return "name"
     }
-    if (user.hasOwnProperty('role')) {
-        if (user.role == "") {
-            return "role"
-        }
+    if ((user.hasOwnProperty('role')) && (user.role == "")) {
+        return "role"
     }
-    if (user.hasOwnProperty('password')) {
-        if (user.password.length <= 4) {
-            return "password"
-        }
+    if ((user.hasOwnProperty('password')) && (user.password.length <= 4)) {
+        return "password"
     }
     if (user.hasOwnProperty('email')) {
         const emailPattern = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gm
