fbb9d9732194059155e42a11c11a2dab9118c486
Shpackk <victorija45@gmail.com>
2021-09-07 15:23:23 +0300

    comments deleted

--- models/permission.js
+++ models/permission.js
@@ -4,13 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Permission extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate({ Role }) {
-      // define association here
       this.belongsToMany(Role, { through: 'RolePermission' })
     }
   };
