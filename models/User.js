9856924adb558bb48ae35ae4eb13e4aaa4695f34
Shpackk <victorija45@gmail.com>
2021-09-08 16:24:43 +0300

    aliases added

--- models/User.js
+++ models/User.js
@@ -5,10 +5,10 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate({ Team, Request, Banlist, Role }) {
-      this.belongsTo(Role, { foreignKey: 'roleId' })
-      this.belongsTo(Team, { foreignKey: 'teamId' })
+      this.belongsTo(Role, { as: 'role', foreignKey: 'roleId' })
+      this.belongsTo(Team, { as: 'team', foreignKey: 'teamId' })
       this.hasOne(Request)
-      this.hasOne(Banlist, { foreignKey: 'userId' })
+      this.hasOne(Banlist, { as: 'banlist', foreignKey: 'userId' })
     }
   };
   User.init({
